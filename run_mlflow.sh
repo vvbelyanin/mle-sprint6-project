f686493b8d2249ca8d447d5b5a92d03566c6479e
vvbelyanin <vv.belyanin@yandex.ru>
2024-09-24 19:27:20 +0000

    sprint_6_project

--- run_mlflow.sh
+++ run_mlflow.sh
@@ -9,7 +9,7 @@ $DB_DESTINATION_HOST:$DB_DESTINATION_PORT/$DB_DESTINATION_NAME
 
 # Start the MLflow server
 mlflow server \
-  --backend-store-uri $POSTGRES_SQL_CONN \   # Use the PostgreSQL backend store URI for experiment tracking
-  --registry-store-uri $POSTGRES_SQL_CONN \  # Use the same PostgreSQL backend for the model registry
-  --default-artifact-root s3://$S3_BUCKET_NAME \  # Specify the default root location in S3 for storing artifacts
-  --no-serve-artifacts   # Disable MLflow's artifact serving feature (since artifacts are stored directly in S3)
+  --backend-store-uri $POSTGRES_SQL_CONN \
+  --registry-store-uri $POSTGRES_SQL_CONN \
+  --default-artifact-root s3://$S3_BUCKET_NAME \
+  --no-serve-artifacts
